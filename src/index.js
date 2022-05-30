0f917374b48fdf9a1f82cd00d57632ed2a62badc
NerovnuiMihail <nerovnuimihail@gmail.com>
2022-05-30 13:32:39 +0300

    create default webpack config

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Post from "./Post";
+import "./styles/style.css";
+import "./styles/style.scss";
+
+const post = new Post('webpac');
+
+console.log(post);
+console.log('work');
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    };
+
+    if (isProd) {
+        config.minimizer = [
+            `...`, 
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ];
+    }
+
+    return config;
+};
+
+module.exports = {
+    context: path.resolve(__dirname,'src'),
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        filename: '[name].[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].bundle.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+              },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
